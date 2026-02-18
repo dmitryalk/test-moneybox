e834293b335371464f47d484bb37efdcf10f07c9
dmitry.a <dmitry_alk@yahoo.com>
2026-02-18 13:44:30 +0000

    Refactoring to move domain logic to domain models

--- src/Moneybox.App/Domain/User.cs
+++ src/Moneybox.App/Domain/User.cs
@@ -4,10 +4,20 @@ namespace Moneybox.App
 {
     public class User
     {
-        public Guid Id { get; set; }
+        public Guid Id { get; init; }
 
-        public string Name { get; set; }
+        public string Name { get; private set; }
 
-        public string Email { get; set; }
+        public string Email { get; private set; }
+
+        public static User Create(string name, string email)
+        {
+            return new User
+            {
+                Id = Guid.NewGuid(),
+                Name = name,
+                Email = email
+            };
+        }
     }
 }
